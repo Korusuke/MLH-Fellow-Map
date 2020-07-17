b0ffc935bd254342249e10e764a8be87ca2c48df
Laveesh Gupta <loveeshgoyal@gmail.com>
2020-07-17 14:14:35 +0530

    Filters setting (#67)
    * Add new styling for the filter
    
    * :bug: Hotfix for right x not restoring map
    
    * Fix: filter styling for different views
    
    Co-authored-by: Ryan Samarakoon <ryan2002.rs@gmail.com>

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,5 +1,6 @@
-import React, { ReactElement } from 'react';
-import { Form, FormGroup, Label, Input } from 'reactstrap';
+import React from 'react';
+import Select from 'react-select';
+import _ from 'lodash';
 
 const Filters = ({
   layers,
@@ -7,34 +8,39 @@ const Filters = ({
 }: {
   layers: { [k in string]: boolean };
   setLayers: (val: { [k in string]: boolean }) => void;
+  layers: { [k: string]: boolean };
+  setLayers: (val: { [k: string]: boolean }) => void;
 }) => {
-  const filterOptions: ReactElement[] = [];
-  Object.keys(layers).map(function (key, index) {
-    filterOptions.push(
-      <FormGroup key={index} check>
-        <Label check>
-          <Input
-            type="checkbox"
-            checked={layers[key]}
-            name={key}
-            onChange={handleLayer}
-          />{' '}
-          {key}
-        </Label>
-      </FormGroup>,
-    );
-  });
+  const selectOptions = _.map(layers, (layer, key) => ({
+    value: key,
+    label: key,
+  }));
 
-  function handleLayer(event: { target: { name: string; checked: boolean } }) {
-    const keyx = event.target.name;
-    const newLayers = { ...layers };
-    newLayers[keyx] = event.target.checked;
+  function handleLayer(
+    event: Array<{ value: string; label: string }> | null | undefined,
+  ) {
+    if (Array.isArray(event) && event.length === 0) event = null;
+    const selectedLayers = _.map(event, layer => layer.label);
+    const newLayers: { [x: string]: boolean } = {};
+    _.forEach(selectOptions, option => {
+      newLayers[option.label] = event
+        ? _.includes(selectedLayers, option.label)
+        : true;
+    });
     setLayers(newLayers);
   }
 
   return (
-    <div id="filters">
-      <Form>{filterOptions}</Form>
+    <div className="container filter row">
+      <div className="col-md-4 offset-md-8 pr-1">
+        <Select
+          onChange={handleLayer}
+          options={selectOptions}
+          isMulti
+          placeholder="Filter based on team"
+          closeMenuOnSelect={false}
+        />
+      </div>
     </div>
   );
 };
